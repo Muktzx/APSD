0b5f7d76036f5805477b5f34ec1b6b56fd8e6dbe
Muktzx <164583279+Muktzx@users.noreply.github.com>
2024-07-18 16:04:46 +0700

    Add files via upload

---
+++ Tugas/Tugas 2/algoritma sederhana.cpp
@@ -0,0 +1,19 @@
+menghitung_luaspanjanglapangan_sepakbola
+panjang lapangan 100
+lebar lapangan 64
+panjang*lebar
+= Luas 
+
+pseudocode
+
+judul
+program luas_lapangan_sepak_bola
+
+deskripsi
+var panjang, lebar, luas, : integer
+
+implementasi
+panjang <- 100
+lebar <- 64
+luas <- panjang*lebar
+PRINT Luas
